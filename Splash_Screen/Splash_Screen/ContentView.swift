75e4aa0f2d4fc1f4c2d15a221f6f4e6731f7dd3e
pan3800 <pan173434@gmail.com>
2024-07-24 15:33:28 +0900

    스플래쉬 화면 만들기

---
+++ Splash_Screen/Splash_Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Splash_Screen
+//
+//  Created by 임채성 on 2024/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Splash_Screen/Splash_Screen/Splash_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Splash_ScreenApp.swift
+//  Splash_Screen
+//
+//  Created by 임채성 on 2024/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Splash_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
