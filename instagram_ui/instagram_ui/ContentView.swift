fab77ee73c88e51ec405d0bd9b86e1929419b77e
pan3800 <pan173434@gmail.com>
2024-07-28 16:25:55 +0900

    프로젝트 생성

---
+++ instagram_ui/instagram_ui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  instagram_ui
+//
+//  Created by 임채성 on 2024/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram_ui/instagram_ui/instagram_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_uiApp.swift
+//  instagram_ui
+//
+//  Created by 임채성 on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct instagram_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
